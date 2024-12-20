0fd19048f39160e691ca87f683ab52494b76c317
Altlog214608 <kimdonghyeon1126@gmail.com>
2024-12-20 17:13:21 +0900

    20241220

--- thisisjava/src/ch05/IndexOfContainsExample.java
+++ thisisjava/src/ch05/IndexOfContainsExample.java
@@ -15,15 +15,15 @@ public class IndexOfContainsExample {
         
                 location = subject.indexOf("자바");
                 if(location != -1){
-                    System.out.println("yes java");
+                    System.out.println("yes 자바");
                 } else {
-                    System.out.println("no java");
+                    System.out.println("no 자바");
                 }
                 boolean result = subject.contains("자바");
                 if(result){
-                    System.out.println("yes java");
+                    System.out.println("yes 자바");
                 } else {
-                    System.out.println("no java");
+                    System.out.println("no 자바");
                 }
                 """;
 //        다른 사람 풀이
@@ -46,10 +46,14 @@ public class IndexOfContainsExample {
 //            }
 //        }
 //
-
-
+        
         //몇 개의 단어 찾을지 사용자 입력 받아서 가장 많이 나온 키워드 추출
         //ex) 사용자 입력 1번에 out, system, 자바
+        //String AAA = "out,System,print"
+        //String[] res = AAA.split(",");
+        //res[0] //out이 들어있음
+        //res[1] //System이 들어있음
+        //res[2] //print가 들어있음
 
         Scanner sc = new Scanner(System.in);
 
@@ -59,8 +63,30 @@ public class IndexOfContainsExample {
         int count = 0;
         int index = Half.indexOf("out");
 
-        String word = sc.nextLine();
+        String word = sc.nextLine()+",";
         String[] wordlist = word.split(",");
+        
+        
+        int max = 0;
+        String maxWord = "";
+
+        for(int i = 0; i < wordlist.length; i++) {
+            count = 0;
+            for (index = Half.indexOf(wordlist[i]); index != -1; index = Half.indexOf(wordlist[i], index + 1)) {
+                count++;
+            }
+            System.out.println(wordlist[i] + " : " + count);
+            if (count > max) {
+                maxWord = wordlist[i];
+                max = count;
+            }
+        }
+        System.out.println("가장 많이 나온단어: "+ "\""+maxWord +"\""+ "의 개수: " +max);
+
+//        for (index = Half.indexOf("out"); index != -1; index = Half.indexOf("out", index + 1)) {
+//            count++;
+//        }
+//        System.out.println(count);
 
 //        while (index != -1) {
 //            count++;
@@ -73,22 +99,6 @@ public class IndexOfContainsExample {
 //            count++;
 //            index = Half.indexOf(wordlist[0], index + 1);
 //        }
-
-        int[] countArray = new int[wordlist.length];
-
-        for(int i = 0; i < wordlist.length; i++) {
-            count = 0;
-            for (index = Half.indexOf(wordlist[i]); index != -1; index = Half.indexOf(wordlist[i], index + 1)) {
-                count++;
-
-            }
-            System.out.println(wordlist[i] + " : " + count);
-            countArray[i] = count;
-        }
-
-//        for (index = Half.indexOf("out"); index != -1; index = Half.indexOf("out", index + 1)) {
-//            count++;
-//        }
-//        System.out.println(count);
+        
     }
 }

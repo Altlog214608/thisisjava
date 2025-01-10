6330f54931f588da1b4433b7c991e8bc0f906df7
Altlog214608 <kimdonghyeon1126@gmail.com>
2025-01-10 14:40:33 +0900

    20250110

--- thisisjava/src/ch06/SportsCar.java
+++ thisisjava/src/ch06/SportsCar.java
@@ -116,6 +116,7 @@ public class SportsCar { //공개 클래스 public
         s1.powerOff();
         System.out.println(s1.sum(1,2,3,4));
 
+        SportsCar.simulate();
         simulate();
 
         SportsCar myCar = new SportsCar();

7062ecbfe8260ad5e0774e1fa2c7c26f1f11f61f
Altlog214608 <kimdonghyeon1126@gmail.com>
2025-01-13 17:45:30 +0900

    20250113

--- thisisjava/src/ch07/Extends_exam.java
+++ thisisjava/src/ch07/Extends_exam.java
@@ -22,6 +22,22 @@ public class Extends_exam {
         sa.flymode = SuperSonicAirplane.NORMAL;
         sa.fly();
         sa.land();
+
+        B b = new B();
+        C c = new C();
+        D d = new D();
+        E e = new E();
+
+        A a1 = b;
+        A a2 = c;
+        A a3 = d;
+        A a4 = e;
+
+        B b1 = d;
+        C c1 = e;
+
+        // B b3 = e;
+        // C c2 = d;
     }
 }
 
@@ -85,6 +101,14 @@ class SuperSonicAirplane extends Ariplane {
         }
     }
 }
+
+class A{}
+class B extends A{}
+class C extends A{}
+class D extends B{}
+class E extends C{}
+
+
 
 
 

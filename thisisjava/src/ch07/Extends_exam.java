a2ddb2be1ec12a535d3b3382182a25ff60e2aab0
Altlog214608 <kimdonghyeon1126@gmail.com>
2025-01-13 17:23:51 +0900

    20250113

--- thisisjava/src/ch07/Extends_exam.java
+++ thisisjava/src/ch07/Extends_exam.java
@@ -13,6 +13,15 @@ public class Extends_exam {
 
         Computer computer = new Computer();
         System.out.println(computer.areaCircle(r));
+
+        SuperSonicAirplane sa = new SuperSonicAirplane();
+        sa.takeOff();
+        sa.fly();
+        sa.flymode = SuperSonicAirplane.SUPERSONIC;
+        sa.fly();
+        sa.flymode = SuperSonicAirplane.NORMAL;
+        sa.fly();
+        sa.land();
     }
 }
 
@@ -49,7 +58,42 @@ class Computer extends Calculator {
     }
 }
 
+class Ariplane {
+    public void land() {
+        System.out.println("착륙합니다");
+    }
+    public void fly() {
+        System.out.println("일반 비행합니다.");
+    }
+    public void takeOff() {
+        System.out.println("이륙 합니다.");
+    }
+}
+
+class SuperSonicAirplane extends Ariplane {
+    public static final int NORMAL = 1;
+    public static final int SUPERSONIC = 2;
+
+    public int flymode = NORMAL;
+
+    @Override
+    public void fly() {
+        if(flymode == SUPERSONIC) {
+            System.out.println("초음속 비행합니다");
+        } else {
+            super.fly();
+        }
+    }
+}
+
+
+
 //다중 상속 불가
 //여러개 클래스를 상속 받는 것 : 다중상속
 //부모 클래스의 생성자가 매개변수가 있다면 -> 자식 생성자 내부에 super(매개변수1, 매개변수2) 작성해야한다
 //부모 클래스의 생성자가 매개변수가 없다면 -> 자식 생성자 내부에 super() 생략 가능
+//super 키워드를 통한 부모 클래스 필드 접근 가능 => super.필드명
+//부모클래스의 필드가 private으로 선언되어 있으면 자식 클래스에서 접근 불가
+//자식클래스에서 부모클래스로 접근하려면 부모클래스에 해당 필드가 public 이나 protected여야함
+//super()는 자식 클래스에서 부모 생성자 호출 방식
+//super.메서드명 형태로 부모클래스 내 메서드 호출 가능

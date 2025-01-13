f0f205a05904c20544283308a149defc4e6d1ce2
Altlog214608 <kimdonghyeon1126@gmail.com>
2025-01-13 16:44:29 +0900

    20250113

--- thisisjava/src/ch07/Extends_exam.java
+++ thisisjava/src/ch07/Extends_exam.java
@@ -7,6 +7,12 @@ public class Extends_exam {
         System.out.println("모델" + smartPhone.model);
         System.out.println("색상" + smartPhone.color);
 
+        int r = 10;
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.areaCircle(r));
+
+        Computer computer = new Computer();
+        System.out.println(computer.areaCircle(r));
     }
 }
 
@@ -28,5 +34,22 @@ class SmartPhone extends Phone {
     }
 }
 
+class Calculator {
+    public double areaCircle(double r) {
+        System.out.println("Cal_area");
+        return 3.14159* r * r;
+    }
+}
+
+class Computer extends Calculator {
+    @Override
+    public double areaCircle(double r){
+        System.out.println("Com_area");
+        return Math.PI * r * r;
+    }
+}
+
 //다중 상속 불가
 //여러개 클래스를 상속 받는 것 : 다중상속
+//부모 클래스의 생성자가 매개변수가 있다면 -> 자식 생성자 내부에 super(매개변수1, 매개변수2) 작성해야한다
+//부모 클래스의 생성자가 매개변수가 없다면 -> 자식 생성자 내부에 super() 생략 가능

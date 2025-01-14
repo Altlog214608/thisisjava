7162d396de6037182755ccef4e3460392965bfe9
Altlog214608 <kimdonghyeon1126@gmail.com>
2025-01-14 09:21:34 +0900

    20250114

--- thisisjava/src/ch07/java_0114.java
+++ thisisjava/src/ch07/java_0114.java
@@ -1,4 +1,12 @@
 package thisisjava.src.ch07;
 
 public class java_0114 {
+    public static void main(String[] args) {
+
+    }
+
+    //자식타입 변수 = (자식타입)부모타입인스턴스; //강제 형변환
+    //부모타입 변수 = 자식인스턴스; // 자동형변환
+    //부모를 상속받은 자식인스턴스가
+    //부모타입으로 자동형변환되면 부모타입에 선언된 필드와 메서드만 사용가능
 }

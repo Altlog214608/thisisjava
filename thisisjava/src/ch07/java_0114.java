ae665502ff9aa62c63d623d59dd53d2a3d40a3c1
Altlog214608 <kimdonghyeon1126@gmail.com>
2025-01-14 09:31:04 +0900

    20250114

--- thisisjava/src/ch07/java_0114.java
+++ thisisjava/src/ch07/java_0114.java
@@ -1,7 +1,17 @@
 package thisisjava.src.ch07;
 
 public class java_0114 {
-    public static void main(String[] args) {
+    public void main(String[] args) {
+        Parent parent = new Child();
+
+        parent.field1 = "data1";
+        parent.method1();
+        parent.method2();
+
+        Child child = (Child) parent;
+
+        child.field2 = "data2";
+        child.method3();
 
     }
 
@@ -9,4 +19,24 @@ public class java_0114 {
     //부모타입 변수 = 자식인스턴스; // 자동형변환
     //부모를 상속받은 자식인스턴스가
     //부모타입으로 자동형변환되면 부모타입에 선언된 필드와 메서드만 사용가능
+
+    class Parent {
+        public String field1;
+
+        public void method1() {
+            System.out.println("Parent-method1");
+        }
+
+        public void method2() {
+            System.out.println("Parent-method2");
+        }
+    }
+    class Child extends Parent{
+        public String field2;
+
+        public void method3(){
+            System.out.println("Child-method3()");
+        }
+
+    }
 }

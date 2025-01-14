e6fd067affd27c3263176dacfc7b7323104017d5
Altlog214608 <kimdonghyeon1126@gmail.com>
2025-01-14 16:30:47 +0900

    20250114

--- thisisjava/src/ch07/Abstract_Exam.java
+++ thisisjava/src/ch07/Abstract_Exam.java
@@ -19,6 +19,11 @@ public abstract class Abstract_Exam {
     //실체클래스는 실제 인스턴스를 생성하여 프로그램 내 상호작용을 실질적으로 하는 클래스
     //추상 클래스는 실체 클래스의 상속용도로 사용
 
+    //추상메서드 선언
+    //abstract void sound(매개변수); //하나의 필드로서 형태만 미리 정의함 _ 추상클래스 내부에
+    //추상메서드 선언하는 이유
+
+
     public static void main(String[] args) {
         SmartPhone2 smartPhone2 = new SmartPhone2("홍길동");
 
